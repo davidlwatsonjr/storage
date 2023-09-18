717e51beb8c621dbebf7ddf5c72be7c894c3c9f8
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-09-17 23:54:07 -0400

    Beef up error handling

--- src/index.js
+++ src/index.js
@@ -38,64 +38,54 @@ app.get("/ping", async (req, res) => {
 
 app.get("/files", async (req, res, next) => {
   const { q } = req.query;
-  try {
-    const { status, data } = await listFiles(q);
-    const { files } = data;
-    res.status(status).send({ count: files.length, files });
-  } catch (err) {
-    next(err);
-  }
+  const { status, data } = await listFiles(q);
+  const { files } = data;
+  res.status(status).send({ count: files.length, files });
 });
 
 app.get("/files/:id", async (req, res, next) => {
   const { id } = req.params;
-  try {
-    const response = await getFile(id);
-    const { status, data } = response;
-    res.status(status).send(data);
-  } catch (err) {
-    next(err);
-  }
+  const response = await getFile(id);
+  const { status, data } = response;
+  res.status(status).send(data);
 });
 
 app.post("/files", async (req, res, next) => {
   const { body, name } = req.body;
-  try {
-    const { status, data } = await uploadFile(body, name);
-    res.status(status).send(data);
-  } catch (err) {
-    next(err);
-  }
+  const { status, data } = await uploadFile(body, name);
+  res.status(status).send(data);
 });
 
 app.put("/files/:id", async (req, res, next) => {
   const { id } = req.params;
   const { body } = req.body;
-  try {
-    const { status, data } = await updateFile(id, body);
-    res.status(status).send(data);
-  } catch (err) {
-    next(err);
-  }
+  const { status, data } = await updateFile(id, body);
+  res.status(status).send(data);
 });
 
 app.delete("/files/:id", async (req, res, next) => {
   const { id } = req.params;
+  const { status, data } = await deleteFile(id);
+  res.status(status).send(data);
+});
+
+app.use((err, req, res, next) => {
   try {
-    const { status, data } = await deleteFile(id);
-    res.status(status).send(data);
-  } catch (err) {
-    next(err);
+    const { response } = err;
+    const { status, data } = response;
+    const { error } = data;
+    const { message } = error;
+    console.error(`${status} - ${message}`);
+    res.status(status).send(message);
+  } catch {
+    throw err;
   }
 });
 
 app.use((err, req, res, next) => {
-  const { response } = err;
-  const { status, data } = response;
-  const { error } = data;
-  const { message } = error;
-  console.error(`${status} - ${message}`);
-  res.status(status).send(message);
+  const { message } = err;
+  console.error(`ERROR - ${message}`);
+  res.status(500).send(message);
 });
 
 const PORT = process.env.PORT || 8080;
